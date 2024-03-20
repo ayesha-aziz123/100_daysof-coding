26cad9a431358292fb9c11a65d226bfc4c34a241
ayesha-aziz123 <163914368+ayesha-aziz123@users.noreply.github.com>
2024-03-20 10:21:53 -0700

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,9 @@
+"use strict";
+// Question 2: Personal Message: Store a person’s name in a variable and print a message to them, like “Hello Asharib, would you like to learn some TypeScript today?”
+// let PersonsName = "Asharib";
+// console.log(`hellow ${PersonsName}, would you like to learn some TypeScript today?`);
+// Question 3: Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+// let personName = "cases";
+// console.log(personName.toUpperCase());
+// console.log(personName.toLowerCase());
+// console.log(personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase());
