8ec479492081ffb29cb1927085500eee6ba4884b
ayesha-aziz123 <163914368+ayesha-aziz123@users.noreply.github.com>
2024-05-06 03:12:44 -0700

    Add files via upload

---
+++ day46.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Q 136 - Use console.log() to debug and track the value of a variable inside a loop.
+for (let i = 1; i <= 5; i++) {
+    console.log(`Iteration ${i}, i value:, i`); // Logs the current iteration number and the value of i
+}
+// Q 137 - Implement a try-catch block to handle potential errors in a block of code.
+try {
+    //intentionally cause an error
+    throw new Error("Somrthing went wrong");
+}
+catch (error) {
+    // console.log(error.message); ///logs the error
+}
+// Q 138 - Describe how to use breakpoints in browser developer tools to debug JavaScript code.
+console.log("Before break point ");
+console.log("This line has a breakpoint set on it in the browser's developer tools");
+console.log("After break point");
---
+++ day47.js
@@ -0,0 +1,24 @@
+"use strict";
+// Q 139 - List three reserved words in JavaScript and create a valid use case for each.
+Object.defineProperty(exports, "__esModule", { value: true });
+// Reserved word: let - used to declare a block-scoped local variable
+let count = 10;
+// Reserved word: if - used to execute a block of code if a specified condition is true
+if (count > 0) {
+    console.log("Count is greater than 0");
+}
+// Reserved word: return - used to exit a function and return a value from that function
+function add(a, b) {
+    return a + b;
+}
+// Demonstrates valid use cases for the reserved words 'let', 'if', and 'return'.
+// Q 140 - Explain the error that occurs when trying to use a reserved word as a variable name.
+// let if = 10 // this line case  a syntax error
+console.log("Using a reserved word as a variable name causes a syntax error in JavaScript.");
+// Q 141 - Discuss the significance of the await reserved word in asynchronous JavaScript.
+let fechSomething = () => { };
+async function fetchData() {
+    const data = await fechSomething();
+    console.log(data);
+}
+console.log("The 'await' keyword allows asynchronous, promise-based behavior to be written in a cleaner, more linear fashion.");
