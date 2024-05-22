90b1024e3de504d8e4c19797c754c433228e9edb
ayesha-aziz123 <moizs4644@gmail.com>
2024-05-22 16:14:21 -0700

    update

---
+++ Day73/Type-Assertion/Type-assertion.ts
@@ -0,0 +1,22 @@
+/*******************
+ * TYPE ASSERTIONS *
+ *******************/
+
+
+let data: any = "1000";
+
+// Type assertion to treat "data" as a string
+console.log((data as string).repeat(2));
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Day78/README.md
@@ -0,0 +1,1 @@
+# 🚀 100 Days of Coding Challenge 🚀
